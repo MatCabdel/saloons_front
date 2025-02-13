32fdde4431efb9f1a85d08b90e809a52d547e859
MatCabdel <chauveau.math@gmail.com>
2025-02-13 15:26:51 +0100

    change architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './general/welcome/welcome.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: AppComponent,
   },
-  {
-    path: 'welcome',
-    component: WelcomeComponent,
-  },
+  { path: 'home', loadChildren: () =>  import('./modules/home/home.module').then(m  => m.HomeModule) },
+  { path: 'auth', loadChildren: () =>  import('./modules/auth/auth.module').then(m  => m.AuthModule) },
 ];
--- src/app/modules/home/welcome/pages/welcome.component.ts
+++ src/app/modules/home/welcome/pages/welcome.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
-  standalone: true,
-  imports: [],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss',
+  styleUrl: './welcome.component.scss'
 })
-export class WelcomeComponent {}
+export class WelcomeComponent {
+
+}
