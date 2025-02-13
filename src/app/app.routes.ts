c83cce00889b2a194d301c6a37a75c44a40cbb26
MatCabdel <chauveau.math@gmail.com>
2025-02-13 12:39:45 +0100

    start project with welcome page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './general/welcome/welcome.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: AppComponent,
+  },
+  {
+    path: 'welcome',
+    component: WelcomeComponent,
   },
 ];
