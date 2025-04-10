5b7df31ba73aeff860d4f8a7db26abb7b7e2e7c5
MatCabdel <chauveau.math@gmail.com>
2025-04-10 17:23:48 +0200

    create call api get saloons

--- src/app/features/saloon/services/saloon-api.service.ts
+++ src/app/features/saloon/services/saloon-api.service.ts
@@ -5,21 +5,19 @@ import { environment } from 'src/environments/environment.development';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SaloonApiService {
-
   private _http = inject(HttpClient);
 
   private readonly _BASE_URL_API = environment.apiUrl;
 
-  getListSaloon(): Observable<Saloon[]>{
+  getListSaloon(): Observable<Saloon[]> {
     const token = localStorage.getItem('token');
-  
-    const headers = {
-      Authorization: `Bearer ${token}`
-    };
-    return this._http.get<Saloon[]>(this._BASE_URL_API + '/saloon', { headers })
-  }
 
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    return this._http.get<Saloon[]>(this._BASE_URL_API + '/saloon', { headers });
+  }
 }
