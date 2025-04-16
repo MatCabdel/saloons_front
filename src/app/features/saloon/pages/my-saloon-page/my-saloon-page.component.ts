302e9670b622f411082209144848e9c3d863ef32
MatCabdel <chauveau.math@gmail.com>
2025-04-16 11:53:11 +0200

    create visitor card in saloons page

--- src/app/features/saloon/pages/my-saloon-page/my-saloon-page.component.ts
+++ src/app/features/saloon/pages/my-saloon-page/my-saloon-page.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavbarComponent } from '../../../../common/components/navbar/navbar.component';
+import { HeaderComponent } from "../../../../common/components/header/header.component";
+import { VisitorCardComponent } from "../../components/visitor-card/visitor-card.component";
+import { Observable } from 'rxjs';
+import { User } from 'src/app/features/user/models/user';
+import { CommonModule } from '@angular/common';
+import { UserService } from 'src/app/features/user/services/user.service';
 
 @Component({
   selector: 'app-my-saloon-page',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeaderComponent, VisitorCardComponent, CommonModule],
   templateUrl: './my-saloon-page.component.html',
   styleUrl: './my-saloon-page.component.scss',
 })
-export class MySaloonPageComponent {}
+export class MySaloonPageComponent {
+
+  private _userService = inject(UserService)
+
+  users$: Observable<User[]> = this._userService.getListUser();
+
+  showModal = true;
+
+  closeModal(): void {
+    this.showModal = false;
+  }
+
+  stopPropagation(event: KeyboardEvent): void {
+    event.stopPropagation();
+  }
+}
