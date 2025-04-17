ebf2a1e2cf0bb8506908211d3f4297da52afa47b
MatCabdel <chauveau.math@gmail.com>
2025-04-17 11:01:17 +0200

    change profil page + nav + style general

---
+++ src/app/features/profil/components/my-profil/my-profil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-profil',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-profil.component.html',
+  styleUrl: './my-profil.component.scss'
+})
+export class MyProfilComponent {
+
+}
--- src/app/features/profil/pages/profil-page/profil-page.component.ts
+++ src/app/features/profil/pages/profil-page/profil-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from 'src/app/common/components/header/header.component';
 import { NavbarComponent } from 'src/app/common/components/navbar/navbar.component';
+import { MyProfilComponent } from "../../components/my-profil/my-profil.component";
 
 @Component({
   selector: 'app-profil-page',
   standalone: true,
-  imports: [NavbarComponent, HeaderComponent],
+  imports: [NavbarComponent, HeaderComponent, MyProfilComponent],
   templateUrl: './profil-page.component.html',
   styleUrl: './profil-page.component.scss',
 })
