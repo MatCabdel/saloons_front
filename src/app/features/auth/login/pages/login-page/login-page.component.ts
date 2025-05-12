043ae8997feb6407ba604b5c281652fdde51421b
MatCabdel <chauveau.math@gmail.com>
2025-05-12 11:49:34 +0200

    start to change all style

--- src/app/features/auth/login/pages/login-page/login-page.component.ts
+++ src/app/features/auth/login/pages/login-page/login-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { HeaderComponent } from 'src/app/common/components/header/header.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [LoginFormComponent, RouterModule],
+  imports: [LoginFormComponent, RouterModule, HeaderComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
