f48193e25bbd88f345d11bf489f272700193eabe
MatCabdel <chauveau.math@gmail.com>
2025-09-03 00:13:13 +0200

    fix route and auth for CD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './features/home/pages/welcome-page/welcome-page.component';
 import { ListSaloonPageComponent } from './features/saloon/pages/list-saloon-page/list-saloon-page.component';
 import { RegisterPageComponent } from './features/auth/register/pages/register-page/register-page.component';
@@ -16,11 +15,8 @@ import { MatchPageComponent } from './features/match/pages/match-page/match-page
 import { ConversationsPageComponent } from './features/conversation/pages/conversations-page/conversations-page.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-  },
-  { path: '/', component: WelcomePageComponent },
+
+  { path: '', component: WelcomePageComponent },
   { path: 'saloons', component: ListSaloonPageComponent, canActivate: [isLoggedInGuard] },
   { path: 'map', component: MapSaloonPageComponent, canActivate: [isLoggedInGuard] },
   { path: 'register', component: RegisterPageComponent },
@@ -33,4 +29,5 @@ export const routes: Routes = [
   { path: 'profil-visitor/:id', component: ProfilVisitorPageComponent, canActivate: [isLoggedInGuard] },
   { path: 'match/:userId1/:userId2', component: MatchPageComponent, canActivate: [isLoggedInGuard] },
   { path: 'messages/:conversationId', component: MessagesPageComponent, canActivate: [isLoggedInGuard] },
+  { path: '**', redirectTo: '' } 
 ];
